71a70db6cc7d21c06e10797f16a2e6787142fecd
m-ghiasi <mahdiye71gh@gmail.com>
2025-01-18 10:16:59 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router";
+import "./App.css";
+
+function App() {
+  return <div>
+
+<Routes>
+  <Route></Route>
+</Routes>
+
+  </div>;
+}
+
+export default App;
